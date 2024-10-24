f9e0a43a22e66b07644cc869c67d742517532c99
soledadrodriguez <68907554+soledadrodriguez@users.noreply.github.com>
2024-10-24 00:31:31 +0000

    [Primer commit]
    - navbar
    -componente card

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { Logo } from "../../img/Star-Wars-Logo-PNG-HD-Outline.png"
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="nav ">
+			<div className="container-fluid ">
+				<img
+					className="ms-5"
+					width="50"
+					height="50"
+					src="https://img.icons8.com/ios/100/star-wars.png"
+					alt="star-wars"
+				/>
+
+						<button className="btn btn-primary dropdown-toggle float-end mt-3" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Favourites</button>
+						<ul className="dropdown-menu">
+							<li><a className="dropdown-item" href="#"></a></li>
+						</ul>
+
 			</div>
 		</nav>
 	);
